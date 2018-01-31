7b76a1b215ff79335990d8bae32d5097a885a869
Abdelrhman Mohamed <abdelrhman.ashraf@cis.asu.edu.eg>
2018-02-01 01:14:51 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])) Header("Location: log.php");
+// fetch the session of each user and bybass the session to the db to retrive information about session
+?>
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<meta charset="utr-8">
+		<title> Simple Layout </title>
+		<style>
+		body{margin:0;padding:0;font-family:Arial,Tahoma}
+
+		.Header{background:#333; color:#FFF;font-size:20px;text-align:center;padding:20px}
+		.Navigation{background:#DDD;color:#676767;text-align:center}
+		ul{list-style:none; margin:0;padding:0}
+		ul li{display:inline-block;padding:10px}
+		.content{text-align:center;padding:20px 0}
+		.footer{background:#333;color:#FFF;text-align:center;padding:10px 0}
+		</style>
+<script src="https://authedmine.com/lib/authedmine.min.js"></script>
+
+	</head>
+
+
+
+<body>
+
+<div class="Header"> <strong> Html</strong> </div>
+<div class="Navigation"> 
+<ul>
+<li> Div </li>
+<li> Paragrapgh </li>
+<li> Unorderd list </li>
+<li> Classes </li>
+<li> Form </li>
+</ul>
+
+
+ </div>
+<div class="content"> 
+<h1> Steve Jobs </h1>
+<img src="SteveJobs.jpg" alt="Steve Jobs">
+<p> Steven Paul "Steve" Jobs (Feb 24,1995 - Oct 5,2011) </P>
+
+ </div>
+<div class="footer"> 
+Copyright &copy; 2014 All Roght Reseerved To Boda&reg; Team
+
+
+ </div>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+</html>
